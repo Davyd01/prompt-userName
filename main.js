cbcaf600cd8ddc5058a7872e9d4d093e41ede3ba
David <davidarutyunov77@gmail.com>
2024-10-24 11:47:48 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// function calculateBirthYear() {
+//   let currentYear = prompt("What is it year now?");
+//   let age = prompt("How old are you?");
+//   let birthYear = currentYear - age;
+//   alert("Your year of birth:" +  birthYear);
+// }
+// calculateBirthYear(); 
+
+
+// let number = Number(prompt("Fill the number please"));
+// let message = '';
+
+// if (number > 0) {
+//   message = '1';
+// } else if (number < 0) {
+//   message = '-1';
+// } else {
+//   message = '0';
+// }
+
+// console.log(message);
+
+
